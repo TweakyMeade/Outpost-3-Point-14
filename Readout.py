3d499623e6caab83bf8182447eb95214e3cbd2e2
Anthony <Anthony>
2022-06-29 22:59:47 +0100

    csv internation

--- Readout.py
+++ Readout.py
@@ -1,20 +1,23 @@
-from tkinter import W
 import weatherhat
 import time
 import datetime
 import csv
-
+flag = True
 outPost = weatherhat.WeatherHAT()
-file = open('test.csv', mode=W)
-recorder = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-recorder.writerow(['Date/Time', 'Temp', 'Humid', "Rain"])
-file.close()
+
 while True:
+    file = open('test.csv', "a")
+    recorder = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    if flag == True:
+        recorder.writerow(['Date/Time', 'Temp', 'Humid', "Rain"])
+        flag = False
     outPost.update(interval=5)
-    print(datetime.datetime.now())
-    print(f"Temp: {outPost.temperature} Degrees Celc")
-    print(f"Humid: {outPost.humidity}%")
-    print(f"Rain: {outPost.rain}")
+    recorder.writerow([datetime.datetime.now(), outPost.temperature, outPost.humidity, outPost.rain])
+    #print(datetime.datetime.now())
+    #print(f"Temp: {outPost.temperature} Degrees Celc")
+    #print(f"Humid: {outPost.humidity}%")
+    #print(f"Rain: {outPost.rain}")
+    file.close()
     time.sleep(5)
 
 

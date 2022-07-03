efe904edbfd264d08e49a1b73692cb0fb0c20eb2
Anthony <Anthony>
2022-07-03 12:34:30 +0100

    Improving the program Untested,

--- databaseInjector.py
+++ databaseInjector.py
@@ -1,24 +1,30 @@
 import datetime 
 import time
 import weatherhat
+import os
+import dotenv
 import paho.mqtt.client as mqtt
 
 def printData(dtI,tpI,hI,rI,wI):
     print(f'"Datetime:{dtI}, "Temp":{tpI}, "Humidity":{hI}, "Rain":{rI}, "Windspeed":{wI}')
+dotenv.load_dotenv()
 
 outPost = weatherhat.WeatherHAT()
 client=mqtt.Client("test")
-client.connect(host="192.168.1.178", port=1883)
+client.connect(host=os.getenv("mqttHost"), port=os.getenv("mqttPort"))
 
 while True:
     outPost.update(interval=5.0)
     datetimeATM= datetime.datetime.timestamp(datetime.datetime.now())
     tempR = outPost.temperature
     humidR = outPost.humidity
+    presureR = outPost.pressure
     windSpeedR = outPost.wind_speed
     windDirectionR = outPost.wind_direction
     rainR = outPost.rain
+    lightR = outPost.lux
+    dewR = outPost.dewpoint
+    totalRainR = outPost.rain_total
     payload = f'{{"Datetime":{datetimeATM}, "Temp":{tempR}, "Humidity":{humidR}, "Rain":{rainR}, "Windspeed":{windSpeedR}, "WindDir":{windDirectionR}}}'
-
     client.publish("Weather",payload)
     time.sleep(5)

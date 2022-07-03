b3c62d93cdb184f1ad701d6ba6a3ecb06b2c8f6e
Anthony <Anthony>
2022-07-03 09:17:19 +0100

    final

--- databaseInjector.py
+++ databaseInjector.py
@@ -1,33 +1,24 @@
-from datetime import date
-import json
+import datetime 
 import time
 import weatherhat
-import csv
-def printData(dI,tI,tpI,hI,rI,wI):
-    print(f'"Date":f{dI}, "Time":{tI}, "Temp":{tpI}, "Humidity":{hI}, "Rain":{rI}, "Windspeed":{wI}')
-def payloadData(dI,tI,tpI,hI,rI,wI):
-    jsonformat = f'"Date":{dI}, "Time":{tI}, "Temp":{tpI}, "Humidity":{hI}, "Rain":{rI}, Windspeed":{wI}'
-    return jsonformat
-        
-outPost = weatherhat.WeatherHAT()
 import paho.mqtt.client as mqtt
-import time
-import datetime
+
+def printData(dtI,tpI,hI,rI,wI):
+    print(f'"Datetime:{dtI}, "Temp":{tpI}, "Humidity":{hI}, "Rain":{rI}, "Windspeed":{wI}')
+
+outPost = weatherhat.WeatherHAT()
 client=mqtt.Client("test")
 client.connect(host="192.168.1.178", port=1883)
 
-for i in range(1,3):
-    dateATM = date.today()
-    timeATM = time.strftime("%H:%M:%S")
+while True:
     outPost.update(interval=5.0)
+    datetimeATM= datetime.datetime.timestamp(datetime.datetime.now())
     tempR = outPost.temperature
     humidR = outPost.humidity
     windSpeedR = outPost.wind_speed
     windDirectionR = outPost.wind_direction
     rainR = outPost.rain
-    payload ="{" + payloadData(dateATM,timeATM,tempR,humidR,rainR,windSpeedR) + "}"
-    print(payload)
-    print("{"+ payload + "}")
-    #printData(dateATM,timeATM,tempR,humidR,rainR,windSpeedR)
-    client.publish("Weather",f"{payload}")
+    payload = f'{{"Datetime":{datetimeATM}, "Temp":{tempR}, "Humidity":{humidR}, "Rain":{rainR}, "Windspeed":{windSpeedR}, "WindDir":{windDirectionR}}}'
+
+    client.publish("Weather",payload)
     time.sleep(5)

ab310415a77b50203b4bf402b5cd8fc67b00381a
Anthony <Anthony>
2022-07-14 13:59:30 +0100

    Bit of discription

--- databaseInjector.py
+++ databaseInjector.py
@@ -2,18 +2,33 @@ import datetime
 import time
 import weatherhat
 import os
-import dotenv
 import paho.mqtt.client as mqtt
+
+"""
+These Variables are configurable.
+I personally like storing server information in .env files but its not required
+Remove dotenv within the code below and plug the ip and port straight into mqttHost and mqttPort
+
+"""
+import dotenv
 dotenv.load_dotenv()
 waitingBuffTime = 5
 userInterval = 30
 mqttHost = str(os.getenv("mqttHost"))
 mqttPort = int(os.getenv("mqttPort"))
+mqttTopic = "Weather"
+
+"""
+Setting up the Weatherhat and MQTT as easily understandable Classes
+"""
 outPost = weatherhat.WeatherHAT()
 client=mqtt.Client()
 client.connect(host=mqttHost, port=mqttPort)
 print(f"Currently connecting to Database {mqttHost} on Port {mqttPort}. \nstart pumping weather data in {waitingBuffTime} seconds with {userInterval} second Intervals")
 time.sleep(waitingBuffTime)
+"""
+while Loop to push data to the server
+"""
 while True:
     outPost.update(interval=userInterval)
     datetimeATM= datetime.datetime.timestamp(datetime.datetime.now())
@@ -27,5 +42,5 @@ while True:
     dewR = outPost.dewpoint
     totalRainR = outPost.rain_total
     payload = f'{{"Datetime":{datetimeATM}, "Temp":{tempR}, "Humidity":{humidR}, "Rain":{rainR}, "Windspeed":{windSpeedR}, "WindDir":{windDirectionR}, "DewPoint":{dewR}, "Light":{lightR}, "Presure":{presureR}}}'
-    client.publish("Weather",payload)
+    client.publish(mqttTopic,payload)
     time.sleep(30)

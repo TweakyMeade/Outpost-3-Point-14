9d9efcf1b43b0fb4ecddce70892375e447532307
Ant <antnymeade@gmail.com>
2022-07-19 20:57:25 +0100

    Update scorchio.py

--- scorchio.py
+++ scorchio.py
@@ -6,7 +6,13 @@ def gen2dict(gen):
     #a lazy method just to convert generator to dictionary
     for i in gen:
         return i
+def textRender(iDict,w,h):
+    hOffset = h
+    for i in iDict.keys():
+        screen.blit(programFont.render(f"{i}: {dictresult[i]}", True, "Red"),(w,hOffset))
+        hOffset += 20
 
+        
 pygame.init()
 pygame.font.init()
 pygame.display.set_caption('Scorchio!')
@@ -20,31 +26,16 @@ arrow = pygame.transform.scale(rawArrow, (50, 70))
 Flag=True
 while Flag:
     initresult = client.query('SELECT * FROM Shed ORDER BY DESC LIMIT 1;')
-    result = initresult.get_points(measurement="Shed")
-    dictresult = gen2dict(result)
-    Temprature = dictresult["Temp"]
-    Humidity = dictresult["Humidity"]
-    Windspeed = dictresult["Windspeed"]
-
+    dictresult = gen2dict(initresult.get_points(measurement="Shed"))
     screen.fill((255,255,255))
-    rTemp=programFont.render(f"Temp: {Temprature}", True, "Black")
-    rHumid=programFont.render(f"Humidity: {Humidity}", True, "Black")
-    rWS=programFont.render(f"Windspeed: {Windspeed}", True, "Black")
-    rWD=programFont.render(f"WindDir: {dictresult['WindDir']}", True, "Black")
-    rDT=programFont.render(f"Datetime: {dictresult['Datetime']}", True, "Black")
-    
+    textRender(dictresult,10,10)
     try:
         rArrow = pygame.transform.rotate(arrow,-dictresult["WindDir"])
         screen.blit(rArrow,(225,300))
     except:
-        print(dictresult['WindDir'])
+        pass
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Flag = False
-    screen.blit(rTemp,(20,10))
-    screen.blit(rHumid,(20,30))
-    screen.blit(rWS,(20,50 ))
-    screen.blit(rWD,(20,70 ))
-    screen.blit(rDT,(20,470 ))
     pygame.display.flip()
